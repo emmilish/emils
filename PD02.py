7ac52fe27423a9bf4d7e8ceddf6041342f706c20
emmilish <biblivsemils@gmail.com>
2024-12-16 13:21:01 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,63 @@
+#2 uzdevums
+n = int(input("ievadi skaitli:"))
+summa = 0
+for i in range(1,1 + n):
+    summa = 1
+   print("skaitļu summa no 1 lidz n ir:", summa)
+   #3 uzdevums
+ n = int(input("ievadi skaitli"))
+    for i in range(1,1 + n):
+ print("skaitļu faktorials no 1 ldz n")
+faktorials = 1
+#4 uzdevums
+n = int(input("Ievadi skaitli:"))
+for i in range(n):
+    print(i)
+    #5 uzdevums
+lenght = int(input("Lenght:"))
+for i in range(lenght, 0, 1):
+    print(1)
+    #6 uzdevums
+    import random
+    min_skaitlis = 1
+max_skaitlis = 100
+iedomats_skaitlis = random.randit(min_skaitlis un max_skaitlis)
+minesanas_ reizes = 1
+while True:
+    minesanas_reizes = 1
+lietotaja_ievade = int(input("Ievadi savu minejumu"))
+if lietotaja_ievade<minesanas_ reizes:
+    print("lielaks")
+    if lietotaja_ievade<minesanas reizes:
+print("mazāks")
+else (uzminets)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
